e7ec7d7b1ac40ba30ac404fa92e998e04c9d08b2
Luiz Gustavo Corrêa <110572628+CoguimA@users.noreply.github.com>
2023-02-06 21:19:41 -0300

    Add files via upload

---
+++ escola.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 07-Fev-2023 às 01:17
+-- Versão do servidor: 10.4.27-MariaDB
+-- versão do PHP: 8.1.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `escola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `curso`
+--
+-- Erro ao ler a estrutura para a tabela escola.curso: #1932 - Table &#039;escola.curso&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela escola.curso: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `escola`.`curso`&#039; na linha 1
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `turma`
+--
+-- Erro ao ler a estrutura para a tabela escola.turma: #1932 - Table &#039;escola.turma&#039; doesn&#039;t exist in engine
+-- Erro ao ler dados para tabela escola.turma: #1064 - Você tem um erro de sintaxe no seu SQL próximo a &#039;FROM `escola`.`turma`&#039; na linha 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
